3fdebf49fe74c90fb6e9bb0e488774f0513eacc0
Artiommm Oaoao <ijezz@List.ru>
2023-10-14 16:29:23 +0300

    lab2

---
+++ lab 2_1/lab 2_1.cpp
@@ -0,0 +1,41 @@
+﻿#include <cstdlib>
+#include <iostream>
+#include <clocale>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	cout << "Вывести сумму + чисел 1 части массива и сумму - чисел 2 части массива" << '\n';
+
+	time_t k;
+	srand(time(&k));
+
+	const int N = 10;
+	float X[N];
+	float s1 = 0.0;
+	float s2 = 0.0;
+
+	for (int i = 0; i < N; i++) {
+		X[i] = float(rand() % 11 - 5);
+
+		if (i == int(N / 2)) { cout << '\n'; }
+		cout << X[i] << ' ';
+
+		if (i < int(N / 2) && X[i] > 0) {
+			s1 += X[i];
+		}
+		if (i >= int(N / 2) && X[i] < 0) {
+			s2 += X[i];
+		}
+	}
+	cout << '\n';
+
+	cout << "Вывод суммы положительных чисел 1 половины массива" << '\n';
+	cout << s1 << '\n';
+
+	cout << "Вывод суммы отрицательных чисел 2 половины массива" << '\n';
+	cout << s2 << '\n';
+}
+
---
+++ lab 2_2/lab 2_2.cpp
@@ -0,0 +1,50 @@
+﻿#include <cstdlib>
+#include <iostream>
+#include <iomanip>
+#include <clocale>
+#include <ctime>
+
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	cout << "Найти сумму элементов побочной диагонали и создать массив из ср.ар. строк" << '\n' << '\n';
+
+	time_t k;
+	srand(time(&k));
+
+	const int N = 8;
+	float avg[N] = {};
+	int arr[N][N] = {};
+	int sumDiag = 0;
+	int sumStr = 0;
+	int count = N;
+
+	cout << "Элементы матрицы:" << '\n';
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			arr[i][j] = rand() % 11;
+			sumStr += arr[i][j];
+			cout.width(3);
+			cout << right << arr[i][j] << ' ';
+		}
+		avg[i] = float(sumStr) / N;
+		cout << '\n';
+	}
+	cout << '\n';
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N - i; j++) {
+			sumDiag += arr[i][j];
+		}
+	}
+
+	cout << "Сумма элементов слева от побочной диагонали = "<< sumDiag << '\n' << '\n';
+	
+	cout << "Массив ср. арифметических строк:" << ' ';
+
+	for (int i = 0; i < N; i++) {
+		cout << avg[i] << ' ';
+	}
+	cout << '\n';
+}
